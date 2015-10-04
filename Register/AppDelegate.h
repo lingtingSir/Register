1eed8b111062ab3577e39be192aa55d89e29becc
lmj <lmj@153.63.210.222.broad.cd.sc.dynamic.163data.com.cn>
2015-10-04 11:33:57 +0800

    Initial Commit

---
+++ Register/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Register
+//
+//  Created by lmj on 15-10-4.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Register/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Register
+//
+//  Created by lmj on 15-10-4.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
